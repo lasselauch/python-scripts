f0823028d11a9fdcca4369d6d6bcd9d7376bdbe8
Lasse Lauch <44460746+lasselauch@users.noreply.github.com>
2018-11-02 15:59:12 +0100

    increment up/down

--- version-up.py
+++ version-up.py
@@ -2,15 +2,17 @@
 #Use at your own risk
 import os, re
 
-def VersionUp(path="", last_version=False):
-    if path == "":
+def VersionUp(path='', only_last=False, increment='up'):
+    if path is None:
         return
 
     if os.name == 'nt':
         pass
+
     pattern = '_(v|V)(\d+)'
-    matches = re.findall(pattern, path)
-    if last_version:
+
+    matches = re.findall(pattern, str(path))
+    if only_last:
         matches = [matches.pop(-1)]
 
     oldpath = path
@@ -21,7 +23,13 @@ def VersionUp(path="", last_version=False):
     for i, m in enumerate(matches):
         v, old = m
         padding = len(old)
-        new = str(int(old)+1).zfill(padding)
+
+        if increment == 'up':
+            increment = int(old)+1
+        if increment == 'down':
+            increment = int(old)-1
+
+        new = str(increment).zfill(padding)
         path = path.replace(old, new)
 
     if not os.path.exists(path):
@@ -31,12 +39,13 @@ def VersionUp(path="", last_version=False):
 
 myString = ".../tesatv01201/Folder_v112/Folder_v01212/TestGeo_V00121.abc"
 myString = "/Volumes/Animation_CG/last_version/_V/scenes/scene_280/Maya/LOS_sc280/cache/alembic/LOS_sc280_sh090_Chuck_V12312.abc"
-myString = r"D:\Dropbox\180425_Adidas\25_AfxOut\SH01\SH01_PreComp_v03"
 myString = r"D:\__WORK\ADIDAS\Adidas_Swim\30_C4d\RotatingVolleyball\RotatingVolleyball_v01.c4d"
+myString = r"D:\Dropbox\180425_Adidas\25_AfxOut\SH01\SH01_PreComp_v03"
 myString = r"D:\Dropbox\180425_Adidas\25_AfxOut\SH04\SH04_PreComp_v05\SH04_PreComp_v05_00000.dpx"
 myString = r"D:\__WORK\UNFOLD\BMW_GKL\02_projects\3D\KRISTALL\Kristall_v09.c4d"
+myString = r"D:\__WORK\ADIDAS\Adidas_Swim\30_C4d\lal\Bra_Setup\Bra_Setup_v02.c4d"
 
-oldpath, newpath = VersionUp(myString, False)
+oldpath, newpath = VersionUp(path=myString, only_last=False, increment='up')
 
 print oldpath
 print newpath
